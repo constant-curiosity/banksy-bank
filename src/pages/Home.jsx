2d98e2e65c5f934ef38daef81a4b7d6bd69a0ae1
name <email@email.com>
2023-07-30 06:05:29 +0000

    Page Layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,28 +8,28 @@ export default function Home() {
   );
 }
 
-// import React, { useState, useEffect } from "react";
+// // import React, { useState, useEffect } from "react";
 
-// import bankLobby from "/home/machine/Banksy_Bank/public/bank_images/bank_Lobby.jpg";
-// import blueCard from "/home/machine/Banksy_Bank/public/bank_images/blue_CC.jpg";
-// import piggyBank from "/home/machine/Banksy_Bank/public/bank_images/blue_Pig_Bank.jpg";
+// // import bankLobby from "/home/machine/Banksy_Bank/public/bank_images/bank_Lobby.jpg";
+// // import blueCard from "/home/machine/Banksy_Bank/public/bank_images/blue_CC.jpg";
+// // import piggyBank from "/home/machine/Banksy_Bank/public/bank_images/blue_Pig_Bank.jpg";
 
-// const imageArr = [bankLobby, blueCard, piggyBank];
+// // const imageArr = [bankLobby, blueCard, piggyBank];
 
-// export default function Home() {
-//   const [count, setCount] = useState(0);
-//   useEffect(() => {
-//     const timer = setInterval(() => {
-//       setCount((count) => count + 1);
-//     }, 3000);
-//     return () => clearInterval(timer);
-//   }, []);
-//   const image = imageArr[count % imageArr.length];
-//   return (
-//     <>
-//       <div id="banner" className="text-center container-fluid">
-//         <img src={image} alt="Bank Images" />
-//       </div>
-//     </>
-//   );
-// }
+// // export default function Home() {
+// //   const [count, setCount] = useState(0);
+// //   useEffect(() => {
+// //     const timer = setInterval(() => {
+// //       setCount((count) => count + 1);
+// //     }, 3000);
+// //     return () => clearInterval(timer);
+// //   }, []);
+// //   const image = imageArr[count % imageArr.length];
+// //   return (
+// //     <>
+// //       <div id="banner" className="text-center container-fluid">
+// //         <img src={image} alt="Bank Images" />
+// //       </div>
+// //     </>
+// //   );
+// // }
