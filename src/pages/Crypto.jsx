2ad0fb57f8f576ff840905b5dcb733e08341be67
name <email@email.com>
2023-08-08 03:27:09 +0000

    Crypto Data Chart

--- src/pages/Crypto.jsx
+++ src/pages/Crypto.jsx
@@ -4,23 +4,34 @@ import { Helmet } from "react-helmet";
 //Use loader data
 
 export default function Crypto() {
-  // const cryptoData = useLoaderData();
+  const cryptoData = useLoaderData();
+  const sparkLine = cryptoData.data.coin.sparkline;
+  const links = cryptoData.data.coin.links;
+  console.log(sparkLine);
+  console.log(links);
+
   return (
     <>
       <Helmet>
         <title>Crypto</title>
       </Helmet>
-      <h1>Crypto</h1>
+      {/* <h1>{sparkLine}</h1> */}
     </>
   );
 }
 
-// export async function loader() {
-// const res = await fetch(
-//   "http://api.coinlayer.com/api/2023-08-05?access_key=832ffcc765bceedb3bb939f68d73c1d3&symbols=XMR"
-// );
-// const data = await res.json();
-// console.log(data);
-// console.log("Loading Crypto page data...");
-// return data;
-// }
+export async function loader() {
+  const xmr = "3mVx2FX_iJFp5";
+  const options = {
+    headers: {
+      "x-access-token":
+        "coinrankingc159eec14135dda16db18dab77de6799801841fdd3976ef9",
+    },
+  };
+  const res = await fetch(
+    `https://api.coinranking.com/v2/coin/${xmr}`,
+    options
+  );
+  const data = await res.json();
+  return data;
+}
