54680dcdd92e06e17f2061b69fbcae84d686cea1
name <email@email.com>
2023-08-16 01:54:58 +0000

    setting pattern values hook form

--- src/components/UI/InputField.jsx
+++ src/components/UI/InputField.jsx
@@ -27,7 +27,8 @@ export default function InputField({
           aria-label={ariaLabel}
           aria-describedby={ariaDescribedBy}
           required={required}
-          {...register(label, { required })}
+          {...register}
+          // {...register(label, { required })}
           className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 lg:text-lg"
         />
       </div>
--- src/components/form/SignupForm.jsx
+++ src/components/form/SignupForm.jsx
@@ -4,7 +4,13 @@ import Button from "../UI/Button";
 import InputField from "../UI/InputField";
 export default function SignupForm() {
   const form = useForm();
-  const { register, control, handleSubmit } = form;
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = form;
+
   const onSubmit = (data) => console.log(data);
   return (
     <>
@@ -18,25 +24,40 @@ export default function SignupForm() {
           type={"text"}
           label={"Email"}
           htmlFor={"email"}
-          register={register}
           aria-label={"Enter your email address"}
-          required
+          register={{
+            ...register("Email", {
+              pattern: {
+                value:
+                  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                message: "Enter a valid email address",
+              },
+            }),
+          }}
         />
+        <span className={""}>{errors.Email?.message}</span>
         <InputField
           type={"text"}
           label={"Password"}
           htmlFor={"password"}
-          register={register}
           aria-label={"Enter your password"}
-          required
+          register={{
+            ...register("Password", {
+              pattern: {
+                value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                message:
+                  "Password must be a min of 8 characters, at least one uppercase letter, one lowercase letter, one number and one special character",
+              },
+            }),
+          }}
         />
+        <span className={""}>{errors.Password?.message}</span>
         <InputField
           type={"text"}
-          label={"Re-enter Passwod"}
+          label={"Re-enter Password"}
           htmlFor={"reEnterPassword"}
-          register={register}
           aria-label={"Re-enter your password"}
-          required
+          register={{ ...register("Re-enter Password", { required: true }) }}
         />
         <div>
           <Button
@@ -53,3 +74,28 @@ export default function SignupForm() {
     </>
   );
 }
+
+// {isValid ? (
+//   <Button
+//     type="submit"
+//     className="mt-5 flex w-full justify-center rounded-md px-3 py-1.5 font-semibold leading-6 text-white shadow-sm hover:bg-banksyHoverRed bg-banksyRed focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+//     aria-label="Account sign up"
+//   >
+//     Sign Up
+//   </Button>
+// ) : (
+//   <Button
+//     type="submit"
+//     disabled
+//     className="mt-5 flex w-full justify-center rounded-md px-3 py-1.5 font-semibold leading-6 text-white shadow-sm bg-banksyHoverRed cursor-not-allowed focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+//     aria-label="Account sign up"
+//   >
+//     Sign Up
+//   </Button>
+// )}
+
+// (includesNumber = /[0-9]/.test(password1)),
+//   (includesLowerCase = /[a-z]/.test(password1)),
+//   (includesUpperCase = /[A-Z]/.test(password1)),
+//   (includesSpecialCharacters =
+//     /[\s~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?()\._]/.test(password1));
