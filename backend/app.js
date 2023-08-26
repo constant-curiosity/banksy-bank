d4332fcf4c62b7303dff2b7dbf16b90ea4f91805
name <email@email.com>
2023-08-26 03:39:15 +0000

    server

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,11 @@
 "Use Strict";
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
