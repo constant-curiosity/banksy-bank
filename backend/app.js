5437046363134b02e53f88afcd9e5a583d9abf2c
name <email@email.com>
2023-08-28 16:23:09 +0000

    clean up

--- backend/app.js
+++ backend/app.js
@@ -32,42 +32,6 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// "Use Strict";
-
-// const express = require("express"),
-//   app = express(),
-//   router = require("./routes/index"),
-//   session = require("express-session"),
-//   mongoStore = require("connect-mongo"),
-//   passport = require("passport");
-// require("./config/database");
-// require("dotenv").config();
-// require("./config/passport");
-
-// app.use(express.static("client"));
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-
-// app.use(
-//   session({
-//     secret: process.env.SECRET,
-//     resave: false,
-//     saveUninitialized: true,
-//     proxy: true,
-//     name: "community-bank",
-//     store: mongoStore.create({
-//       mongoUrl: process.env.DB_STRING,
-//       collection: "sessions",
-//     }),
-//     cookie: {
-//       httpOnly: true,
-//       secure: true,
-//       maxAge: 1000 * 60 * 60 * 48,
-//       sameSite: "none",
-//     },
-//   })
-// );
-
 // app.use(passport.initialize());
 // app.use(passport.session());
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -138,23 +138,3 @@ module.exports = {
   ],
   plugins: [require("@headlessui/tailwindcss")],
 };
-
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         "Branda-yolq": ["BrandaYolq", "sans-serif"],
-//       },
-//       colors: {
-//         banksyRed: "#B30000",
-//         banksyHoverRed: "#FF6666",
-//         banksyBGHeader: "#333333",
-//         banksyGray: "#808080",
-//         banksyFooter: "#CCCCCC",
-//       },
-//     },
-//   },
-//   plugins: [],
-// };
