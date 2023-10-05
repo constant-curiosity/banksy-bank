35976e8db01504f5eae2fee125a36017cae1dc85
name <email@email.com>
2023-10-05 04:03:54 +0000

    account layout

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -35,44 +35,69 @@ const transactions = [
     amount: "$ 190.90",
     link: "#",
   },
+  {
+    transactionID: "#78901234",
+    user: "Mara Pacemaker",
+    item: "Ortovox Backpack 40l",
+    amount: "$ 150.00",
+    link: "#",
+  },
+  {
+    transactionID: "#78901234",
+    user: "Mara Pacemaker",
+    item: "Ortovox Backpack 40l",
+    amount: "$ 150.00",
+    link: "#",
+  },
+  {
+    transactionID: "#78901234",
+    user: "Mara Pacemaker",
+    item: "Ortovox Backpack 40l",
+    amount: "$ 150.00",
+    link: "#",
+  },
 ];
 
 export default function Account() {
   return (
-    <Card>
-      <Flex justifyContent="start" className="space-x-2">
-        <Title>Purchases</Title>
-        <Badge color="gray">8</Badge>
-      </Flex>
-      <Text className="mt-2">Overview of this month's purchases</Text>
+    <div className="flex justify-center">
+      <div className="flex justify-center items-center border-2 border-gray-500 my-4 mx-4 relative w-full overflow-hidden rounded-3xl sm:px-12 lg:max-w-[60rem] ">
+        <Card className="items-center mt-11 mb-11">
+          <Flex justifyContent="start" className="space-x-2">
+            <Title>Purchases</Title>
+            <Badge color="gray">{transactions.length}</Badge>
+          </Flex>
+          <Text className="mt-2">Overview of this month's purchases</Text>
 
-      <Table className="mt-6">
-        <TableHead>
-          <TableRow>
-            <TableHeaderCell>Transaction ID</TableHeaderCell>
-            <TableHeaderCell>User</TableHeaderCell>
-            <TableHeaderCell>Item</TableHeaderCell>
-            <TableHeaderCell className="text-right">Amount</TableHeaderCell>
-            <TableHeaderCell>Link</TableHeaderCell>
-          </TableRow>
-        </TableHead>
+          <Table className="mt-6">
+            <TableHead>
+              <TableRow>
+                <TableHeaderCell>Transaction ID</TableHeaderCell>
+                <TableHeaderCell>User</TableHeaderCell>
+                <TableHeaderCell>Item</TableHeaderCell>
+                <TableHeaderCell className="text-right">Amount</TableHeaderCell>
+                <TableHeaderCell>Link</TableHeaderCell>
+              </TableRow>
+            </TableHead>
 
-        <TableBody>
-          {transactions.map((item) => (
-            <TableRow key={item.transactionID}>
-              <TableCell>{item.transactionID}</TableCell>
-              <TableCell>{item.user}</TableCell>
-              <TableCell>{item.item}</TableCell>
-              <TableCell className="text-right">{item.amount}</TableCell>
-              <TableCell>
-                <Button size="xs" variant="secondary" color="gray">
-                  See details
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Card>
+            <TableBody>
+              {transactions.map((item) => (
+                <TableRow key={item.transactionID}>
+                  <TableCell>{item.transactionID}</TableCell>
+                  <TableCell>{item.user}</TableCell>
+                  <TableCell>{item.item}</TableCell>
+                  <TableCell className="text-right">{item.amount}</TableCell>
+                  <TableCell>
+                    <Button size="xs" variant="secondary" color="gray">
+                      See details
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Card>
+      </div>
+    </div>
   );
 }
